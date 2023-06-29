eaa375be9392cbd7ac898f5d69bbf534788de1bd
OoThan <mgoothan.psnl@gmail.com>
2023-06-29 10:07:53 +0630

    add some features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct HelloResponse {
+    message: String,
+}
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(HelloResponse {
+        message: "Hello, world!".to_owned(),
+    })
+}
+
+#[post("/echo")]
+async fn echo(body: web::Json<HelloResponse>) -> impl Responder {
+    HttpResponse::Ok().json(body.into_inner())
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    
 }
